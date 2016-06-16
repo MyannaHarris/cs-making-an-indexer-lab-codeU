8287bfb857695b16bc320d6f680b564dd7cc2f08
Myanna Harris <myharris13@gmail.com>
2016-06-15 21:27:31 -0700

    Done.

--- javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
@@ -37,7 +37,12 @@ public class TermCounter {
 	 */
 	public int size() {
         // TODO: fill this in.
-		return -1;
+                int sum = 0;
+                for(int i: map.values())
+                {
+                  sum += i;
+                }
+		return sum;
 	}
 
 	/**
